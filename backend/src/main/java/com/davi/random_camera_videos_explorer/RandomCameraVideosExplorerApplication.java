272d94da93618b017e0a4ecf2675a297808cc910
Davi Siqueira <davisvsiqueira@gmail.com>
2024-11-19 13:42:27 -0300

    chore: initial commit

---
+++ backend/src/main/java/com/davi/random_camera_videos_explorer/RandomCameraVideosExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.davi.random_camera_videos_explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomCameraVideosExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomCameraVideosExplorerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/davi/random_camera_videos_explorer/RandomCameraVideosExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.random_camera_videos_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomCameraVideosExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
