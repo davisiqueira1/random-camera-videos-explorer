c9992c786f1a0641c0b4949dde9744ed3f576a76
Davi Siqueira <davisvsiqueira@gmail.com>
2024-11-20 02:51:37 -0300

    feat: add initial app logic using dummy data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,37 @@
 import './App.css'
+import {VideoContainer} from "./VideoContainer.jsx";
+import {Component} from "react";
+import {dummyData} from "./utils/dummyData.js";
 
-function App() {
-  return (
-      <div>
-          <h1>Hello world!</h1>
-      </div>
-  )
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            fetchVideoDetails: () => {
+                const data = dummyData[Math.floor(Math.random() * dummyData.length)]
+                console.log(data)
+                this.setState({
+                    videoDetails: data
+                })
+            },
+            videoDetails: null,
+        }
+    }
+
+    componentDidMount() {
+        this.state.fetchVideoDetails()
+    }
+
+    render() {
+        if (this.state.videoDetails === null) return <div>Loading...</div>
+
+        return (
+            <div>
+                <h1>{this.state.videoDetails["videoId"]}</h1>
+                <VideoContainer videoInfo={this.state.videoDetails} fetchNextVideo={this.state.fetchVideoDetails}/>
+            </div>
+        )
+    }
 }
 
 export default App
