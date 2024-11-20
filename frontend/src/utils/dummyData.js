612fcdbc7f30643229847d5bdf866053c00fd979
Davi Siqueira <davisvsiqueira@gmail.com>
2024-11-20 13:24:03 -0300

    chore: add more dummy data

--- frontend/src/utils/dummyData.js
+++ frontend/src/utils/dummyData.js
@@ -22,5 +22,25 @@ export const dummyData = [
         "title": "IMG_2392",
         "publishDate": "2024-06-11T11:42:48",
         "views": 2
-    }
+    },
+    {"videoId":"Wfx4Mbzi-o4","title":"img 282","publishDate":"2009-10-05T06:48:12","views":118},
+    {"videoId":"JKJf1iusziU","title":"Img168","publishDate":"2016-02-23T07:34:42","views":522},
+    {"videoId":"KPsyLk6O5e0","title":"IMG621","publishDate":"2013-05-12T15:28:57","views":126},
+    {"videoId":"LkvNr3teBJc","title":"img649.MOV","publishDate":"2010-05-17T08:25:42","views":104},
+    {"videoId":"Nu9aj-2dN2U","title":"IMG 71","publishDate":"2022-04-28T09:02:20","views":10},
+    {"videoId":"2InjZqwlM4A","title":"KETERLALUAN‼️\uD83D\uDE2D CUKUP MAKAN NASI PADANG SUDAH BERHAGIA","publishDate":"2020-08-16T10:18:43","views":3089},
+    {"videoId":"aITG2YK0iR4","title":"img 565","publishDate":"2019-06-26T17:25:52","views":18},
+    {"videoId":"q-ULac8HjEM","title":"IMG 918","publishDate":"2021-07-03T11:33:02","views":23},
+    {"videoId":"cOwV-zHsE1M","title":"IMG_904","publishDate":"2017-03-31T16:12:12","views":27},
+    {"videoId":"1omgmC5LqG0","title":"İnca IMG-329 Gaming Mouse","publishDate":"2018-05-25T06:23:57","views":3014},
+    {"videoId":"GikaOwvVEz8","title":"IMG 650","publishDate":"2012-11-30T12:08:41","views":31},
+    {"videoId":"w7J3MpAzlqQ","title":"İnca IMG365MS Emin","publishDate":"2018-02-11T14:58:52","views":101},
+    {"videoId":"ZcfygEgByJs","title":"batera, musico IMG_256","publishDate":"2019-02-20T14:35:15","views":3},
+    {"videoId":"PDvUoQqiocs","title":"IMG 839","publishDate":"2021-08-08T09:30:30","views":8},
+    {"videoId":"Lq28XcwyQcM","title":"IMG_809 (17 Dec 2009).AVI","publishDate":"2010-01-14T17:30:07","views":51},
+    {"videoId":"iwSlPM6Wv0w","title":"♥☆ ♥Happy ♥New ♥ Year♥☆ ♥","publishDate":"2013-12-30T13:14:30","views":553},
+    {"videoId":"EyP6LV3YhpY","title":"Drawing Practice | IMG_101 : Doggo","publishDate":"2023-04-06T09:00:10","views":39},
+    {"videoId":"AFNUFc5FmAo","title":"2023.2.15 IMG 526","publishDate":"2023-04-13T11:43:43","views":4},
+    {"videoId":"CFful7N90oM","title":"VIDEO VIRAL TERBARU MASIH ANGET|| NO PW","publishDate":"2023-10-13T13:26:35","views":4112},
+    {"videoId":"gw7skTdKLVE","title":"t3 IMG 764","publishDate":"2023-06-26T12:37:58","views":1}
 ]
