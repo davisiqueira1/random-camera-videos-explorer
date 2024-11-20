e8c1c9660b1622304e44748db595d9d16f6982c0
Davi Siqueira <davisvsiqueira@gmail.com>
2024-11-20 01:58:43 -0300

    feat: add some dummy video data

---
+++ frontend/src/utils/dummyData.js
@@ -0,0 +1,26 @@
+export const dummyData = [
+    {
+        "videoId": "S66L6aUxRTo",
+        "title": "Pgm5 img100",
+        "publishDate": "2024-11-20T03:59:44",
+        "views": 5
+    },
+    {
+        "videoId": "Ncz0DTSHpvY",
+        "title": "Img499",
+        "publishDate": "2013-09-18T11:10:48",
+        "views": 14
+    },
+    {
+        "videoId": "jDSiVgF9eQA",
+        "title": "IMG_1002",
+        "publishDate": "2011-01-12T10:50:48",
+        "views": 10
+    },
+    {
+        "videoId": "R3XrdSoZM6E",
+        "title": "IMG_2392",
+        "publishDate": "2024-06-11T11:42:48",
+        "views": 2
+    }
+]
